cf2c51656a672984f791adcb546c41272297aff4
ahmedalhelali <ahmedalhelali.dev@gmail.com>
2023-07-22 23:17:11 +0300

    models classes added

---
+++ lib/core/src/models/response.freezed.dart
@@ -0,0 +1,430 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'response.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Response _$ResponseFromJson(Map<String, dynamic> json) {
+  return _Response.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Response {
+  int? get currentPage => throw _privateConstructorUsedError;
+  List<User>? get data => throw _privateConstructorUsedError;
+  String? get firstPageUrl => throw _privateConstructorUsedError;
+  int? get from => throw _privateConstructorUsedError;
+  int? get lastPage => throw _privateConstructorUsedError;
+  String? get lastPageUrl => throw _privateConstructorUsedError;
+  List<Link>? get links => throw _privateConstructorUsedError;
+  String? get nextPageUrl => throw _privateConstructorUsedError;
+  String? get path => throw _privateConstructorUsedError;
+  int? get parPage => throw _privateConstructorUsedError;
+  String? get prevPageUrl => throw _privateConstructorUsedError;
+  int? get to => throw _privateConstructorUsedError;
+  int? get total => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ResponseCopyWith<Response> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ResponseCopyWith<$Res> {
+  factory $ResponseCopyWith(Response value, $Res Function(Response) then) =
+      _$ResponseCopyWithImpl<$Res, Response>;
+  @useResult
+  $Res call(
+      {int? currentPage,
+      List<User>? data,
+      String? firstPageUrl,
+      int? from,
+      int? lastPage,
+      String? lastPageUrl,
+      List<Link>? links,
+      String? nextPageUrl,
+      String? path,
+      int? parPage,
+      String? prevPageUrl,
+      int? to,
+      int? total});
+}
+
+/// @nodoc
+class _$ResponseCopyWithImpl<$Res, $Val extends Response>
+    implements $ResponseCopyWith<$Res> {
+  _$ResponseCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? currentPage = freezed,
+    Object? data = freezed,
+    Object? firstPageUrl = freezed,
+    Object? from = freezed,
+    Object? lastPage = freezed,
+    Object? lastPageUrl = freezed,
+    Object? links = freezed,
+    Object? nextPageUrl = freezed,
+    Object? path = freezed,
+    Object? parPage = freezed,
+    Object? prevPageUrl = freezed,
+    Object? to = freezed,
+    Object? total = freezed,
+  }) {
+    return _then(_value.copyWith(
+      currentPage: freezed == currentPage
+          ? _value.currentPage
+          : currentPage // ignore: cast_nullable_to_non_nullable
+              as int?,
+      data: freezed == data
+          ? _value.data
+          : data // ignore: cast_nullable_to_non_nullable
+              as List<User>?,
+      firstPageUrl: freezed == firstPageUrl
+          ? _value.firstPageUrl
+          : firstPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      from: freezed == from
+          ? _value.from
+          : from // ignore: cast_nullable_to_non_nullable
+              as int?,
+      lastPage: freezed == lastPage
+          ? _value.lastPage
+          : lastPage // ignore: cast_nullable_to_non_nullable
+              as int?,
+      lastPageUrl: freezed == lastPageUrl
+          ? _value.lastPageUrl
+          : lastPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      links: freezed == links
+          ? _value.links
+          : links // ignore: cast_nullable_to_non_nullable
+              as List<Link>?,
+      nextPageUrl: freezed == nextPageUrl
+          ? _value.nextPageUrl
+          : nextPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      path: freezed == path
+          ? _value.path
+          : path // ignore: cast_nullable_to_non_nullable
+              as String?,
+      parPage: freezed == parPage
+          ? _value.parPage
+          : parPage // ignore: cast_nullable_to_non_nullable
+              as int?,
+      prevPageUrl: freezed == prevPageUrl
+          ? _value.prevPageUrl
+          : prevPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      to: freezed == to
+          ? _value.to
+          : to // ignore: cast_nullable_to_non_nullable
+              as int?,
+      total: freezed == total
+          ? _value.total
+          : total // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_ResponseCopyWith<$Res> implements $ResponseCopyWith<$Res> {
+  factory _$$_ResponseCopyWith(
+          _$_Response value, $Res Function(_$_Response) then) =
+      __$$_ResponseCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int? currentPage,
+      List<User>? data,
+      String? firstPageUrl,
+      int? from,
+      int? lastPage,
+      String? lastPageUrl,
+      List<Link>? links,
+      String? nextPageUrl,
+      String? path,
+      int? parPage,
+      String? prevPageUrl,
+      int? to,
+      int? total});
+}
+
+/// @nodoc
+class __$$_ResponseCopyWithImpl<$Res>
+    extends _$ResponseCopyWithImpl<$Res, _$_Response>
+    implements _$$_ResponseCopyWith<$Res> {
+  __$$_ResponseCopyWithImpl(
+      _$_Response _value, $Res Function(_$_Response) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? currentPage = freezed,
+    Object? data = freezed,
+    Object? firstPageUrl = freezed,
+    Object? from = freezed,
+    Object? lastPage = freezed,
+    Object? lastPageUrl = freezed,
+    Object? links = freezed,
+    Object? nextPageUrl = freezed,
+    Object? path = freezed,
+    Object? parPage = freezed,
+    Object? prevPageUrl = freezed,
+    Object? to = freezed,
+    Object? total = freezed,
+  }) {
+    return _then(_$_Response(
+      currentPage: freezed == currentPage
+          ? _value.currentPage
+          : currentPage // ignore: cast_nullable_to_non_nullable
+              as int?,
+      data: freezed == data
+          ? _value._data
+          : data // ignore: cast_nullable_to_non_nullable
+              as List<User>?,
+      firstPageUrl: freezed == firstPageUrl
+          ? _value.firstPageUrl
+          : firstPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      from: freezed == from
+          ? _value.from
+          : from // ignore: cast_nullable_to_non_nullable
+              as int?,
+      lastPage: freezed == lastPage
+          ? _value.lastPage
+          : lastPage // ignore: cast_nullable_to_non_nullable
+              as int?,
+      lastPageUrl: freezed == lastPageUrl
+          ? _value.lastPageUrl
+          : lastPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      links: freezed == links
+          ? _value._links
+          : links // ignore: cast_nullable_to_non_nullable
+              as List<Link>?,
+      nextPageUrl: freezed == nextPageUrl
+          ? _value.nextPageUrl
+          : nextPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      path: freezed == path
+          ? _value.path
+          : path // ignore: cast_nullable_to_non_nullable
+              as String?,
+      parPage: freezed == parPage
+          ? _value.parPage
+          : parPage // ignore: cast_nullable_to_non_nullable
+              as int?,
+      prevPageUrl: freezed == prevPageUrl
+          ? _value.prevPageUrl
+          : prevPageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      to: freezed == to
+          ? _value.to
+          : to // ignore: cast_nullable_to_non_nullable
+              as int?,
+      total: freezed == total
+          ? _value.total
+          : total // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Response implements _Response {
+  const _$_Response(
+      {required this.currentPage,
+      required final List<User>? data,
+      required this.firstPageUrl,
+      required this.from,
+      required this.lastPage,
+      required this.lastPageUrl,
+      required final List<Link>? links,
+      required this.nextPageUrl,
+      required this.path,
+      required this.parPage,
+      required this.prevPageUrl,
+      required this.to,
+      required this.total})
+      : _data = data,
+        _links = links;
+
+  factory _$_Response.fromJson(Map<String, dynamic> json) =>
+      _$$_ResponseFromJson(json);
+
+  @override
+  final int? currentPage;
+  final List<User>? _data;
+  @override
+  List<User>? get data {
+    final value = _data;
+    if (value == null) return null;
+    if (_data is EqualUnmodifiableListView) return _data;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(value);
+  }
+
+  @override
+  final String? firstPageUrl;
+  @override
+  final int? from;
+  @override
+  final int? lastPage;
+  @override
+  final String? lastPageUrl;
+  final List<Link>? _links;
+  @override
+  List<Link>? get links {
+    final value = _links;
+    if (value == null) return null;
+    if (_links is EqualUnmodifiableListView) return _links;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(value);
+  }
+
+  @override
+  final String? nextPageUrl;
+  @override
+  final String? path;
+  @override
+  final int? parPage;
+  @override
+  final String? prevPageUrl;
+  @override
+  final int? to;
+  @override
+  final int? total;
+
+  @override
+  String toString() {
+    return 'Response(currentPage: $currentPage, data: $data, firstPageUrl: $firstPageUrl, from: $from, lastPage: $lastPage, lastPageUrl: $lastPageUrl, links: $links, nextPageUrl: $nextPageUrl, path: $path, parPage: $parPage, prevPageUrl: $prevPageUrl, to: $to, total: $total)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Response &&
+            (identical(other.currentPage, currentPage) ||
+                other.currentPage == currentPage) &&
+            const DeepCollectionEquality().equals(other._data, _data) &&
+            (identical(other.firstPageUrl, firstPageUrl) ||
+                other.firstPageUrl == firstPageUrl) &&
+            (identical(other.from, from) || other.from == from) &&
+            (identical(other.lastPage, lastPage) ||
+                other.lastPage == lastPage) &&
+            (identical(other.lastPageUrl, lastPageUrl) ||
+                other.lastPageUrl == lastPageUrl) &&
+            const DeepCollectionEquality().equals(other._links, _links) &&
+            (identical(other.nextPageUrl, nextPageUrl) ||
+                other.nextPageUrl == nextPageUrl) &&
+            (identical(other.path, path) || other.path == path) &&
+            (identical(other.parPage, parPage) || other.parPage == parPage) &&
+            (identical(other.prevPageUrl, prevPageUrl) ||
+                other.prevPageUrl == prevPageUrl) &&
+            (identical(other.to, to) || other.to == to) &&
+            (identical(other.total, total) || other.total == total));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      currentPage,
+      const DeepCollectionEquality().hash(_data),
+      firstPageUrl,
+      from,
+      lastPage,
+      lastPageUrl,
+      const DeepCollectionEquality().hash(_links),
+      nextPageUrl,
+      path,
+      parPage,
+      prevPageUrl,
+      to,
+      total);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ResponseCopyWith<_$_Response> get copyWith =>
+      __$$_ResponseCopyWithImpl<_$_Response>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_ResponseToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Response implements Response {
+  const factory _Response(
+      {required final int? currentPage,
+      required final List<User>? data,
+      required final String? firstPageUrl,
+      required final int? from,
+      required final int? lastPage,
+      required final String? lastPageUrl,
+      required final List<Link>? links,
+      required final String? nextPageUrl,
+      required final String? path,
+      required final int? parPage,
+      required final String? prevPageUrl,
+      required final int? to,
+      required final int? total}) = _$_Response;
+
+  factory _Response.fromJson(Map<String, dynamic> json) = _$_Response.fromJson;
+
+  @override
+  int? get currentPage;
+  @override
+  List<User>? get data;
+  @override
+  String? get firstPageUrl;
+  @override
+  int? get from;
+  @override
+  int? get lastPage;
+  @override
+  String? get lastPageUrl;
+  @override
+  List<Link>? get links;
+  @override
+  String? get nextPageUrl;
+  @override
+  String? get path;
+  @override
+  int? get parPage;
+  @override
+  String? get prevPageUrl;
+  @override
+  int? get to;
+  @override
+  int? get total;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ResponseCopyWith<_$_Response> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/core/src/models/response.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Response _$$_ResponseFromJson(Map<String, dynamic> json) => _$_Response(
+      currentPage: json['currentPage'] as int?,
+      data: (json['data'] as List<dynamic>?)
+          ?.map((e) => User.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      firstPageUrl: json['firstPageUrl'] as String?,
+      from: json['from'] as int?,
+      lastPage: json['lastPage'] as int?,
+      lastPageUrl: json['lastPageUrl'] as String?,
+      links: (json['links'] as List<dynamic>?)
+          ?.map((e) => Link.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      nextPageUrl: json['nextPageUrl'] as String?,
+      path: json['path'] as String?,
+      parPage: json['parPage'] as int?,
+      prevPageUrl: json['prevPageUrl'] as String?,
+      to: json['to'] as int?,
+      total: json['total'] as int?,
+    );
+
+Map<String, dynamic> _$$_ResponseToJson(_$_Response instance) =>
+    <String, dynamic>{
+      'currentPage': instance.currentPage,
+      'data': instance.data,
+      'firstPageUrl': instance.firstPageUrl,
+      'from': instance.from,
+      'lastPage': instance.lastPage,
+      'lastPageUrl': instance.lastPageUrl,
+      'links': instance.links,
+      'nextPageUrl': instance.nextPageUrl,
+      'path': instance.path,
+      'parPage': instance.parPage,
+      'prevPageUrl': instance.prevPageUrl,
+      'to': instance.to,
+      'total': instance.total,
+    };
